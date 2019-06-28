3c1390d5a3b73d5a09a0360f3521aa00949a66ce
andres-martin <andresmartin266@gmail.com>
2019-06-28 11:26:05 -0500

    [feature] to_dict() and save() public methods created

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-''' class BaseModel that defines all common attributes/methods for other classes '''
+''' class BaseModel that defines all
+    common attributes/methods for other classes '''
 import json
 import uuid
 from datetime import datetime
@@ -18,3 +19,20 @@ class BaseModel:
         ''' str representation '''
         mg = "[BaseModel] ({}) {}"
         return mg.format(self.id, self.__dict__)
+
+    def save(self):
+        ''' updates the public instance attribute updated_at '''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        ''' returns a dictionary with all keys/values
+            of __dict__ of the instance '''
+        custom_dic = {'__class__': __class__.__name__}
+        created_at_iso = self.created_at.isoformat()
+        updated_at_iso = self.updated_at.isoformat()
+        isos = {'created_at': created_at_iso, 'updated_at': updated_at_iso}
+        custom_dic.update(self.__dict__)
+        custom_dic.update(isos)
+        # self.__dict__.update(custom_dic)
+        # dest = {**custom_dic, **self.__dict__}
+        return custom_dic

464b815b8e3c78afcbca75486707208730978ed4
andres-martin <andresmartin266@gmail.com>
2019-06-28 21:35:05 +0000

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,14 @@ from datetime import datetime
 class BaseModel:
     '''int common attributes '''
     # uniq_id = uuid.uuid4()
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         ''' variables '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            [setattr(self, key, value) for key, value in kwargs.items()]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         ''' str representation '''

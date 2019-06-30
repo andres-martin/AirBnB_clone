b995626f3b7d76afb0b3d9636de39e4541d9d151
andres-martin <andresmartin266@gmail.com>
2019-06-30 18:54:01 -0500

    User model created, file storage updated, missing console.py to be updated

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,10 @@
 
 
 from models.base_model import BaseModel
+from models.user import User
 from models.engine.file_storage import FileStorage
 
 
+clases = {"BaseModel": BaseModel, "FileStorage": FileStorage, "User": User}
 storage = FileStorage()
 storage.reload()
-clases = {"BaseModel": BaseModel, "FileStorage": FileStorage}
